e98be3c216ee74d1bff4044b1d7c5799227d38eb
s0561033 <s0561033@htw-berlin.de>
2020-11-01 12:33:40 +0100

    update GUI

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using ScottPlot;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp2.Model;
+using WpfApp2.ViewModel;
 
 namespace WpfApp2
 {
@@ -25,13 +29,24 @@ namespace WpfApp2
         {
             InitializeComponent();
 
+            //Temporary
+
+            Freq = 1;
+            Ampl = 5;
+            Duration = 1m;
+            Rate = 9600;
             TestFunc();
+            Console.WriteLine(637397472332016471 + 9995906);
+            Console.WriteLine($"Freq:{Freq} Ampl:{Ampl} Duration: {Duration} Rate: {Rate}");
+
+            Console.WriteLine($"GetAmplSine {GetAmplSine(2500000)}");
+            
         }
 
         public void TestFunc()
         {
 
-            int pointCount = 51;
+            int pointCount = 50;
             double[] x = DataGen.Consecutive(pointCount);
             double[] sin = DataGen.Sin(pointCount);
             double[] cos = DataGen.Cos(pointCount);
@@ -44,20 +59,86 @@ namespace WpfApp2
             //plt.SaveFig("PlotTypes_Scatter_CustomizeLines.png");
 
         }
+        public long OffsetFreq, Freq,TickRate,Rate;
+        public double OffsetAmpl, Ampl;
+        public decimal Tick,Duration;
+
+
+
+        public void initiateWithGUI()
+        {
+
+            //Freq = Convert.ToInt64(txtFreq.Text);
+            //Ampl = Convert.ToDouble(txtAmp.Text);
+        }
 
         private void sldFreq_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            //Slider slider = sender as Slider;
+            //Console.WriteLine((sender as Slider).Value);
+            OffsetFreq = (long)(sender as Slider).Value;
         }
 
         private void sldAmp_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            OffsetAmpl = (sender as Slider).Value;
         }
 
         private void btnReset_Click(object sender, RoutedEventArgs e)
         {
+           
+            
+            TickRate = (long)Math.Round(1e7m / Rate,0);
+            Console.WriteLine("Tick " + TickRate);
+            //if (rdoSine.IsChecked == true)
+            if (true)
+            {
+                //Should combine Wavefrom in Datagenerate?
+                //Sine mySine = new Sine(Freq, Ampl, 9600);
+                //DataGenerate aData = new DataGenerate(mySine,10);
+                LinkedList<long> TimeStampList = new LinkedList<long>();
+                LinkedList<double> AmplList = new LinkedList<double>();
+                DateTime start = DateTime.Now;
+                Console.WriteLine(TimeStampList.ToString());
+                Console.WriteLine(AmplList.ToString());
 
+                StringBuilder sb = new StringBuilder();
+                for (long i = 0; i < Rate * Duration; i++)
+                {
+                    //TimeStampList.AddFirst(i*Tick);
+                    //AmplList.AddFirst(GetAmplSine(i));
+                    //Console.WriteLine(start.Ticks);
+                    //Console.WriteLine(Math.Round(i * Tick,0));
+                    //sb.AppendLine(Convert.ToString(start.Ticks + i*Tick));
+                    sb.AppendLine(String.Format("{0} {1} {2}",i,start.Ticks + i * TickRate,GetAmplSine(i*TickRate)));
+                    
+                };
+                //Console.WriteLine(sb);
+                //Console.WriteLine(TimeStampList.First);
+                //Console.WriteLine(AmplList.First);
+
+                //string path = @"C:\Users\Admin\Desktop\Testwriting.txt";
+                string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Testwriting.txt";
+
+              
+                File.AppendAllText(filePath, sb.ToString());
+
+            }
         }
+
+        public double GetAmplSine(long attime)
+        {
+            
+            //Console.WriteLine("TEst");
+            //Console.WriteLine(Freq);
+            //Console.WriteLine(Ampl*Math.Sin(2 * Math.PI*Freq*0.25));
+            return Ampl * Math.Sin(2*Math.PI*Freq*attime/1e7);
+        }
+
+        public double GetValueSawtooth(long attime)
+        {
+            return 0;
+        }
+
     }
 }
---
+++ WpfApp2/WpfApp2/Model/Sine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Model
+{
+    public class Sine : BaseWave
+    {
+        public Sine(long freq = 0, double ampl = 0, int tick = 0) : base(freq, ampl, tick) { }
+        public override double generateAmpY(double X)
+        {
+            return Math.Sin(X);
+        }
+    }
+
+}
